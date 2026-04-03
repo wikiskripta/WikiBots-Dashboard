82dff3504c4dc15814aeb97d15d727e326913572
Jan Štěch <5vmufx4rbcf9s7vyd2xqc3n0ky8ciu@bots.bitbucket.org>
2026-04-03 23:30:27 +0200

    Renamed one .env constant

--- Models/UserManager.php
+++ Models/UserManager.php
@@ -49,7 +49,7 @@ class UserManager
             'wsdb_session='.$_COOKIE['wsdb_session']
         ];
 
-        $url = $_ENV['WIKI_LOGIN_API'] . '?' . http_build_query($params);
+        $url = $_ENV['WIKI_API'] . '?' . http_build_query($params);
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_COOKIE, implode(';', $cookies));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
